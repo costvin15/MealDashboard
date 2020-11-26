a8c247be96b116d5f629f0c7a43271f37a46650c
Vinicius Castro <costacastrovinicius7@gmail.com>
2020-11-26 16:04:01 -0300

    feature(frontend/pages/auth/register): Register page improved
    Provider improved

--- frontend/src/pages/Auth/Signup/provider.js
+++ frontend/src/pages/Auth/Signup/provider.js
@@ -1,8 +1,8 @@
 import {api, user} from '../../../helpers'
 
 export const Provider = {
-  performLogin: async ({email, password}) => {
-    const {data: {token, ...data}} = await api.post('/signin', {email, senha: password})
+  performSignUp: async ({name, email, password}) => {
+    const {data: {token, ...data}} = await api.post('/auth/register', {name, email, password})
     user.renewToken({token, data})
   }
 }
