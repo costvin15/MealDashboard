c0919ca1eca902f06a4fd649aff036f963703cb0
Vinicius Castro <costacastrovinicius7@gmail.com>
2020-11-26 15:06:36 -0300

    feature(backend/auth/register): Register route improved

--- backend/src/controllers/index.js
+++ backend/src/controllers/index.js
@@ -1,5 +1,2 @@
 export {default as Home} from "./home";
 export {default as Auth} from "./auth";
-export {default as Peoples} from "./peoples";
-export {default as Partners} from "./partners";
-export {default as Projects} from "./projects";
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,9 +4,6 @@ import jwt from "jsonwebtoken";
 import {
     Home,
     Auth,
-    Peoples,
-    Partners,
-    Projects,
 } from "./controllers";
 
 const routes = express.Router();
@@ -44,31 +41,8 @@ const VerifyJWT = (request, response, next) => {
 };
 
 routes.get("/", Home.home);
-routes.get("/download/:id", Home.download);
 
 routes.post("/auth/login", Auth.login);
-routes.post("/auth/register", VerifyJWT, Auth.register);
-routes.get("/users/", VerifyJWT, Auth.getUsers);
-
-routes.post("/peoples/", VerifyJWT, Peoples.createPeople);
-routes.get("/peoples/", Peoples.getPeoples);
-routes.delete("/peoples/:id", VerifyJWT, Peoples.deletePeople);
-
-routes.post("/peoples/type/", VerifyJWT, Peoples.createType);
-routes.get("/peoples/type/", Peoples.getTypes);
-routes.delete("/peoples/type/:id", VerifyJWT, Peoples.deleteType);
-
-routes.post("/partners/", VerifyJWT, Partners.createPartner);
-routes.put("/partners/", VerifyJWT, Partners.updatePartner);
-routes.get("/partners/", Partners.getPartners);
-routes.delete("/partners/:id", VerifyJWT, Partners.deletePartner);
-
-routes.post("/partners/type", VerifyJWT, Partners.createPartnerCategory);
-routes.get("/partners/type", Partners.getPartnerType);
-routes.delete("/partners/type/:id", VerifyJWT, Partners.deletePartnerType);
-
-routes.post("/projects/", VerifyJWT, Projects.createProject);
-routes.get("/projects/", Projects.getProjects);
-routes.get("/projects/highlighted", Projects.getHighlightedProjects);
+routes.post("/auth/register", Auth.register);
 
 export default routes;
