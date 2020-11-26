95d161aa366236a02953e4fba2425464812fd111
Vinicius Castro <costacastrovinicius7@gmail.com>
2020-11-26 17:55:06 -0300

    feature(frontend/auth/login): Login page improved
    Provider improved

--- frontend/src/pages/Auth/Login/provider.js
+++ frontend/src/pages/Auth/Login/provider.js
@@ -2,7 +2,7 @@ import {api, user} from '../../../helpers'
 
 export const Provider = {
   performLogin: async ({email, password}) => {
-    const {data: {token, ...data}} = await api.post('/signin', {email, senha: password})
+    const {data: {token, ...data}} = await api.post('/auth/login', {email, password})
     user.renewToken({token, data})
   }
 }
